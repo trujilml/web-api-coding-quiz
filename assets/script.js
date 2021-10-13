fa971d79f291303de0c0b2a1fac3776add475e9e
trujilml <mickeytrj@gmail.com>
2021-10-13 13:41:23 -0500

    MAJOR CODE RESTRUCTURE - modified multiple functions, deleted other js file, simplified html to house js functions that illustrate quiz details and allow functions to start working, modify and edit if needed

--- assets/script.js
+++ assets/script.js
@@ -1,206 +1,194 @@
-//figure out function for start quiz, various questions, score for correct answers and incorrect answers + 
-//loss of game time, localStorage for saved score, and demo of leaderboard at end of site. all followed by the required timer.
-//and play again button for game
-//game timer only lasts 100 seconds, incorrect answers recieve a loss of 10 seconds for the user
+
+//game timer only lasts 100 seconds, incorrect answers recieve a loss of 20 seconds for the user
 //100000 milliseconds for 100 seconds - timer, timeleft
 
 //lists all questions choices and answers in a variable - 4 choices max, 1 answer only
-//refer javascript questions from mod 3 and online sources highlighting javascript basics
-//10 questions - correct answers earn 15 points, incorrect answers lose 10 seconds on the timer running in the quiz
-//moved to script.js from original file, reference notes
 //B is a choice never used for the questions correct answer at all! 
 
-// .style.display = none; would be easier that 'hide' class element - try to implement it if allowed
-//figure out if answer button is necessary for the questions when quizzed through - looks like method is click function or implemented elsewhere
-
-const startButton = document.getElementById("start");
-const backButton = document.getElementById("back");
-const nextButton = document.getElementById("next");
-// const submitAnswerButton = document.getElementById("submit-it");
-const submitButton = document.getElementById("submit");
-const clearButton = document.getElementById("clear");
-
-// const questions = document.getElementById("questions");
-const choices = document.getElementById("choices");
-
-let getNewQuestions, randomizeQuestionFormat;
-
-var questions = [
+const questions = [
 
     {
-    question: "What does a comment look like in JavaScript?",
-    choices: ["'This is a comment in JavaScript!'","<!--This a comment in JavaScript!-->", "//This is a comment in JavaScript!", "This is a comment in JavaScript!"],
-    answer: "//This is a comment in JavaScript!"
-         //answer is C
+        questionTitle: "What does a comment look like in JavaScript?",
+        choices: ["'This is a comment in JavaScript!'","!This a comment in JavaScript!", "//This is a comment in JavaScript!", "This is a comment in JavaScript!"],
+        answer: "//This is a comment in JavaScript!" //answer is C
     },
-    
+        
     {
-    question: "Arrays are a special type of objects in JavaScript. What can they store exactly in them?",
-    choices: ["Strings", "Booleans", "Other arrays", "All of the Above"],
-    answer: "All of the Above"
-    //answer is D
+        questionTitle: "How do you declare a function?",
+        choices: ["Function name()", "Class = Function()", "Method Function = ()", "var function = function()"],
+        answer: "Function name()" //answer is A
     },
 
     {
-    question: "What is 'null' exactly?",
-    choices: ["Null represents the absence of a value.","Null is a variable representing what is within a function.", "Null represents the number 0.", "Null means nothing in JavaScript."],
-    answer: "Null represents the absence of a value."
+        questionTitle: "Arrays are a special type of objects in JavaScript. What can they store exactly in them?",
+        choices: ["Strings", "Booleans", "Other arrays", "All of the Above"],
+        answer: "All of the Above"
+        //answer is D
+    },
+
+    {
+        questionTitle: "Which boolean operator represents 'And'?",
+        choices: ["&&", "||", "?!", "*"],
+        answer: "&&"
         //answer is A
     },
-
+        
     {
-    question: "How is a conditional statement in JavaScript declared?",
-    choices: ["The for loop", "The var function statement", "The while loop", "The if else statement"],
-    answer: "The if else statement"
-      //answer is D
-    },
-
-
-    {
-    question: "How do you declare a function?",
-    choices: ["Function name()", "Class = Function()", "Method Function = ()", "var function = function()"],
-    answer: "Function name()"
-    //answer is A
+        questionTitle: "Which statements are true about functions in JavaScript?",
+        choices: ["Functions must always have an identifier.", "Functions cannot be reused.", "None of the above.", "Functions cannot use outside variables in their own functions."],
+        answer: "None of the above." //answer is C
     },
 
     {
-    question: "What is the object method in JavaScript?",
-    choices: ["The object method houses various arrays for a function to use.", "The object method creates objects.", "The object method is an object with a function associated with the method.", "The object method involves an object that is inserted within a function."],
-    answer: "The object method is an object with a function associated with the method.",
-    //answer is C
+        questionTitle: "What is 'null' exactly?",
+        choices: ["Null represents the absence of a value.","Null is a variable representing what is within a function.", "Null represents the number 0.", "Null means nothing in JavaScript."],
+        answer: "Null represents the absence of a value." //answer is A
     },
-
-    {
-    question: "Which boolean operator represents 'And'?",
-    choices: ["&&", "||", "?!", "*"],
-    answer: "&&"
-         //answer is A
-     },
-
-    {
-    question: "Which statements are true about functions in JavaScript?",
-    choices: ["Functions must always have an identifier.", "Functions cannot be reused.", "None of the above.", "Functions cannot use outside variables in their own functions."],
-    answer: "None of the above."
-     //answer is C
-    },
-
-    {
-    question: "What operator is used to assign a value to a variable?",
-    choices: ["Nothing!","=", "?", "+="],
-    answer: "="
-         //answer is A
-     },
     
     {
-    question: "A variable not declared within a function but outside of a function is known as a...",
-    choices: ["An undefined variable","A local variable", "A global variable", "A variable"],
-    answer: "A global variable"
-     //answer is C
+        questionTitle: "What is the object method in JavaScript?",
+        choices: ["The object method houses various arrays for a function to use.", "The object method creates objects.", "The object method is an object with a function associated with the method.", "The object method involves an object that is inserted within a function."],
+        answer: "The object method is an object with a function associated with the method.", //answer is C
+    },
+    
+    {
+        questionTitle: "How is a conditional statement in JavaScript declared?",
+        choices: ["The for loop", "The var function statement", "The while loop", "The if else statement"],
+        answer: "The if else statement" //answer is D
     },
 
-];
+    {
+        questionTitle: "What operator is used to assign a value to a variable?",
+        choices: ["Nothing!","=", "?", "+="],
+        answer: "="
+        //answer is A
+    },        
 
-//timer displays time but does not currently function 
-var timeLeft = 100;
-var wrongAnswerTime = -10; 
+    {
+        questionTitle: "A variable not declared within a function but outside of a function is known as a...",
+        choices: ["An undefined variable","A local variable", "A global variable", "A variable"],
+        answer: "A global variable" //answer is C
+    },
 
-function timer() {
-    setInterval(function() { 
-    document.getElementById("timer").innerHTML=timeLeft + " seconds";
-    }), 1000; //1000 milliseconds allow for timeLeft to run its respective 100 seconds
-}
- if (timeLeft === 0) {
-    alert("GAME OVER!")
-};
+]
+//min totalscore of quiz is 0, maximum score user makes is up to 400
+var totalScore = 0;
+var nextQuestion = -1; 
+var timeLeft = 0;
+var quizTimer;
 
 function startQuiz() {
-    startButton.classList.add("hide");
-    randomizeQuestionFormat = questions.sort(() => Math.random() - 2); //change if question size is too big
-    questions.classList.remove("hide");
-    getNewQuestions = 0;
-    nextNewQuestion();
-    nextButton.classList.remove("hide");
-}
 
+    timeLeft = 100;
+    document.getElementById("timeLeft").innerHTML=timeLeft + " seconds";
 
-function nextNewQuestion() {
-    score(randomizeQuestionFormat[nextNewQuestion]);
-}
-
-
-var totalScore = 0;
-var correctAnswer = 20;
-var highestScore = 200;
-
-function score(questions){
-
-     for (var i = 0; i < questions.length; i++) {
-         score.textContent = questions;
-         if (answer.correct) {
-            correctAnswer.innerHTML = "Correct!";
-             totalScore += correctAnswer;
-             nextButton.dataset.correct = answer.correct
-         }
-         if (!answer.correct) {
-            correctAnswer.innerHTML = "Incorrect!";
-            timeLeft -= wrongAnswerTime;
-           timeLeft = timeLeft-wrongAnswerTime;
-         }
+    quizTimer = setInterval(function() {
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML=timeLeft + " seconds";
+        if (timeLeft <= 0) {
+            clearInterval(quizTimer);
+            window.alert("THE QUIZ IS OVER!");
+            quizOver(); //1000 milliseconds allow for timeLeft to run its respective 100 seconds
         }
-};
+        }, 1000);
 
+        nextNewQuestion();
+    }
 
+//last page - users three initals is saved on local storage as MVP and their score as highscore respectively
+function quizOver() {
+    clearInterval(quizTimer);
 
+    var finalQuizDetails = `
+    <h2>Great job!</h2>
+    <p>Your final score is ` + totalScore + `!</p>
+    <p>Please enter your initials to submit your score in the highscore chart.</p>
+    <input type="text" id="MVP" class="initials" maxlength="3" required>
+    <button onclick="saveHighscore()" class="highscore-btn">Submit</button>`;
 
-
-// var score = function() {
-//     if(this.getNewQuestions().questions(answer)) {
-//         correctAnswer.innerHTML = "Correct!";
-//     totalScore += correctAnswer;
-// }   else if (!this.getNewQuestions().questions(answer)) {
-//     correctAnswer.innerHTML = "Incorrect!";
-//     timeLeft = timeLeft-wrongAnswerTime;
-// }
-// };
+    document.getElementById("startpage").innerHTML = finalQuizDetails;
+}
 
 
 function saveHighscore() {
+    localStorage.setItem("Highscore", totalScore);
+    localStorage.setItem("MVP", document.getElementById('MVP').value);
 
-    var localQuizStorage = "quizscore";
-    var userInitials = "";
-    var value = totalScore; 
-
-    userInitals = localQuizStorage + totalScore.value;
-    value = [userInitials, totalScore]
-
-    if (!localStorage.length) {
-    localStorage.setItem(userInitials, "quizscore"[totalScore]);
-    }
-
-    document.getElementById("submit").onclick = saveHighscore;
+    getHighscore();
 }
 
+function getHighscore() {
+    var finalQuizDetails = `
+    <h2>` + localStorage.getItem("MVP") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("Highscore") + `</h1><br >
+
+    <button onclick="clearHighscore()" class="clear-btn">Clear Score and Play Again!</button>
+    <button onclick="resetGame()" class="reset-btn">Just Play Again!</button>
+
+    `;
+    document.getElementById("startpage").innerHTML = finalQuizDetails;
+}
+
+// clear function clears high score from the user's local storage and brings the user back to the start page
+function clearHighscore(){
+    localStorage.setItem("Highscore", "");
+    localStorage.setItem("MVP", "");
+
+    resetGame();
+}    
+
+//reset function puts user back to the beginning page and allows them to play the quiz again!
+function resetGame() {
+    clearInterval(quizTimer);
+    score = 0;
+    nextQuestion = -1;
+    timeLeft = 0;
+    quizTimer = null;
+
+    document.getElementById("timeLeft").innerHTML=timeLeft + " seconds";
+
+    var finalQuizDetails = `
+    <h2>The Coding Quiz of Knowledge</h2>
+    <p>
+    Welcome to The Coding Quiz of Knowledge. You will be quizzed on your knowledge of JavaScript coding. 
+    Click the "Start Quiz" button to begin. Please try to answer the questions within the 100 second time limit. 
+    Correct answers give you 40 points! Incorrect answers will penalize your time by 25 seconds! Have fun!
+    </p>
+    <button onclick = "startQuiz()" class = "submit-btn">Start Quiz</button>`;
+
+    document.getElementById("startpage").innerHTML = finalQuizDetails;
+}
+
+function wrongAnswer() {
+    timeLeft -= 25;
+    nextNewQuestion();
+}
+
+function correctAnswer() {
+    totalScore += 40;
+    nextNewQuestion();
+}
+
+function nextNewQuestion() {
+    nextQuestion++;
+
+    if (nextQuestion > questions.length - 1) {
+        quizOver();
+        return;
+    }
+
+    var finalQuizDetails = "<h2>" + questions[nextQuestion].questionTitle + "</h2>"
 
 
-// function clearHighscores() {
-//     window.localStorage.removeItem(userInitials, "quizscore"[totalScore]);
-//     window.location.reload();
-
-//     document.getElementById("clear").onclick = clearHighscores;
-// }
-
-
-
-
-startButton.addEventListener = ("click", startQuiz);
-startButton.addEventListener = ("click", timer);
-
-// submitAnswerButton.onclick = saveAnswer;
-
-submitButton.onclick = saveHighscore; 
-saveHighscore();
-// clearButton.onclick = clearHighscores;
-// clearHighscores();
-
-// console.log(startQuiz());
-// startQuiz();
+    for (var i = 0; i < questions[nextQuestion].choices.length; i++) {
+        var choiceButton = "<button onclick = \"[answerchoice]\">[CHOICE]</button>";
+        choiceButton = choiceButton.replace("[CHOICE]", questions[nextQuestion].choices[i]);
+            if (questions[nextQuestion].choices[i] === questions[nextQuestion].answer) {
+                choiceButton = choiceButton.replace("[answerchoice]", "correctAnswer()");
+            } else {
+                choiceButton = choiceButton.replace("[answerchoice]", "wrongAnswer()");
+            }
+        finalQuizDetails += choiceButton
+    }
+        document.getElementById("startpage").innerHTML = finalQuizDetails;
+}
